629954cbedf71caf4ffe4dd5466ef55f7a477c82
harold2rendon <142748727+harold2rendon@users.noreply.github.com>
2026-03-03 15:46:59 -0500

    preparcial-1

---
+++ Preparcial-01/costo_reserva.exs
@@ -0,0 +1,50 @@
+defmodule Hotel do
+
+  def main do
+  noches = Util.leer_entero("Ingrese el número de noches: ")
+
+  tipo_cliente = ("Ingrese el tipo de cliente (frecuente, corporativo, ocasional): ")
+  |> IO.gets()
+  |> String.trim()
+  |> String.downcase()
+  |> String.to_atom()
+
+  recargo_temporada = Util.leer_entero("Ingrese la temporada (1 = temporada alta, 2 = temporada baja): ")
+
+  {tarifa_base, subtotal, descuento, recargo_temporada, total} =
+    calcular(noches, tipo_cliente, recargo_temporada)
+
+    Util.imprimir("Tarifa: #{tarifa_base}")
+    Util.imprimir("Subtotal: #{subtotal}")
+    Util.imprimir("Descuento: #{descuento}")
+    Util.imprimir("Recargo: #{recargo_temporada}")
+    Util.imprimir("Total a pagar: #{total}")
+end
+
+def tarifa(noches) when noches <= 2, do: 120000
+def tarifa(noches) when noches >= 3 and noches <= 5, do: 100000
+def tarifa(noches) when noches > 5, do: 85000
+
+def descuento(:frecuente, subtotal), do: div(subtotal * 20, 100)
+def descuento(:corporativo, subtotal), do: div(subtotal * 15, 100)
+def descuento(:ocasional, _), do: 0
+def descuento(_, _), do: 0
+
+def calcular(noches, tipo_cliente, recargo_temporada) do
+  tarifa_base = tarifa(noches)
+  subtotal = tarifa_base * noches
+  descuento = descuento(tipo_cliente, subtotal)
+  subtotal2 = subtotal - descuento
+
+  recargo_temporada = cond do
+    recargo_temporada == 1 -> div(subtotal2 * 20, 100)
+    recargo_temporada == 2 -> 0
+    true -> 0
+  end
+
+  total = subtotal2 + recargo_temporada
+  {tarifa_base, subtotal, descuento, recargo_temporada, total}
+end
+end
+
+Hotel.main()
